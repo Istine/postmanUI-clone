e7251a7568f19878151f057d51a99e92ac6093c0
Imanche Sabastine <imanchesebastian@gmail.com>
2020-06-29 07:56:59 -0700

    changed break point for desktop view

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,18 @@ import Head from "next/head";
 import { deepOrange, grey } from "@material-ui/core/colors";
 import Container from "@material-ui/core/Container";
 import CssBaseline from "@material-ui/core/CssBaseline/CssBaseline";
-import { Box, TextField, Checkbox, Button } from "@material-ui/core";
+import {
+  Box,
+  TextField,
+  Checkbox,
+  Button,
+  Typography,
+} from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
 import { useMediaQuery } from "@material-ui/core";
 
 const Home: React.FunctionComponent = () => {
-  const query = useMediaQuery("(max-width: 635px)");
+  const query = useMediaQuery("(max-width: 991px)");
   return (
     <div>
       <Head>
@@ -16,10 +22,10 @@ const Home: React.FunctionComponent = () => {
         <link rel="icon" href="/favicon.ico" />
         <script src="https://kit.fontawesome.com/ae9bd935ec.js" defer></script>
       </Head>
-      
-        <React.Fragment>
-          <CssBaseline />
-          {query ? (
+
+      <React.Fragment>
+        <CssBaseline />
+        {query ? (
           <StyledContainer maxWidth="sm">
             <Box height="620px" padding="10px 20px">
               <Box
@@ -124,9 +130,182 @@ const Home: React.FunctionComponent = () => {
               </StyledButtonGoogle>
             </Box>
           </StyledContainer>
-          ) : <h1>Hello World!</h1>}
-        </React.Fragment>
-      
+        ) : (
+          <StyledContainerDesktop maxWidth="md">
+            <Box width="50%" height="100%" position="relative">
+              <Typography
+                component="div"
+                style={{
+                  background: "url(./postman.jpg)",
+                  width: "100%",
+                  height: "100%",
+                  backgroundSize: "cover",
+                  backgroundRepeat: "no-repeat",
+                  marginTop: "0",
+                  marginLeft: "0",
+                  position: "absolute",
+                  padding:'20px 0'
+                }}
+              >
+                <Box
+                  display="flex"
+                  flexDirection='column'
+                  alignItems='center'
+                  width="100%"
+                  height="auto"
+                  justifyContent="center"
+                  position='relative'
+                >
+                  <Typography
+                    component="div"
+                    style={{
+                      background: "url(./logo.png)",
+                      width: "250px",
+                      height: "100px",
+                      backgroundSize: "contain",
+                      backgroundRepeat: "no-repeat",
+                      marginTop: "0",
+                      marginLeft: "0",
+                    }}
+                  >
+                  </Typography>
+                  <Typography
+                    component="div"
+                    style={{
+                    width:'100%',
+                    alignSelf:'center',
+                    display:'flex',
+                    flexDirection:'column',
+                    alignItems:'center'
+                  }}
+                >
+                  <h2 style={{color: 'white', textAlign:'center'}}>Why Sign Up?</h2>
+                  <ul style={{color:'white', opacity:'.95', fontSize:'13px', width:'400px'}}>
+                    <li style={{margin:'10px 0'}}>Organize all your API development within Postman Workspaces</li>
+                    <li style={{margin:'10px 0'}}>Sync Your Postman data across devices</li>
+                    <li style={{margin:'10px 0'}}>Backup your data to the Postman cloud</li>
+                    <li style={{margin:'10px 0'}}>It's free!</li>
+                  </ul>
+                </Typography>
+                </Box>
+              </Typography>
+            </Box>
+            <Box width="50%" height="100%">
+              <Typography
+                component="div"
+                style={{
+                  backgroundColor: "white",
+                  height: "100%",
+                  MozBackgroundSize: "contain",
+                  padding:'20px'
+                }}
+              >
+              <Box
+                width="100%"
+                height="50px"
+                display="flex"
+                margin="0"
+                alignItems="center"
+                justifyContent="space-between"
+              >
+                <h2 style={{ margin: "10px 0" }}>Create Account</h2>
+                <label style={{ fontSize: "12px", fontWeight: "bold" }}>
+                  <span style={{ color: "orange" }}>Sign in</span> and Instead?
+                </label>
+              </Box>
+              <label style={{ fontSize: "13px", fontWeight: "bold" }}>
+                Email
+              </label>
+              <StyledTextField
+                id="outlined-size-small"
+                variant="outlined"
+                size="small"
+                fullWidth
+              />
+              <label style={{ fontSize: "13px", fontWeight: "bold" }}>
+                Username
+              </label>
+              <StyledTextField
+                id="outlined-size-small"
+                variant="outlined"
+                size="small"
+                fullWidth
+              ></StyledTextField>
+              <Box
+                width="100%"
+                height="10px"
+                display="flex"
+                justifyContent="space-between"
+              >
+                <label style={{ fontSize: "13px", fontWeight: "bold" }}>
+                  Password
+                </label>
+                <a style={{ color: "#ccc", cursor: "pointer" }}>SHOW</a>
+              </Box>
+              <StyledTextField
+                id="outlined-size-small"
+                variant="outlined"
+                size="small"
+                fullWidth
+                type="password"
+              ></StyledTextField>
+              <Box
+                width="100%"
+                height="50px"
+                display="flex"
+                alignItems="center"
+              >
+                <StyledCheckBoxGrey
+                  checked={false}
+                  name="checkedB"
+                  color="default"
+                />
+                <label style={{ fontSize: "12px", fontWeight: "bold" }}>
+                  I agree to the{" "}
+                  <span style={{ color: "orange" }}>Terms of Use</span> and{" "}
+                  <span style={{ color: "orange" }}>Privacy Policy</span>
+                </label>
+              </Box>
+              <Box
+                width="100%"
+                height="50px"
+                display="flex"
+                alignItems="center"
+              >
+                <StyledCheckBox
+                  checked={true}
+                  name="checkedB"
+                  color="default"
+                />
+                <label style={{ fontSize: "12px", fontWeight: "bold" }}>
+                  Keep me signed in
+                </label>
+              </Box>
+              <StyledButton variant="contained" color="primary" fullWidth>
+                Create free account
+              </StyledButton>
+              <Box
+                width="100%"
+                height="20px"
+                display="flex"
+                justifyContent="center"
+                margin="15px 0"
+              >
+                <span style={{ color: "#aaa" }}>vs</span>
+              </Box>
+              <StyledButtonGoogle variant="contained" color="primary" fullWidth>
+                <i
+                  style={{ position: "absolute", left: "10px" }}
+                  className="fab fa-google"
+                ></i>
+                Sign up with Google
+              </StyledButtonGoogle>
+              </Typography>
+            </Box>
+          </StyledContainerDesktop>
+        )}
+      </React.Fragment>
+
       <style jsx global>{`
         body {
           margin: 0;
@@ -146,6 +325,15 @@ const StyledContainer = withStyles({
   root: {
     backgroundColor: "white",
     height: "100vh",
+  },
+})(Container);
+
+const StyledContainerDesktop = withStyles({
+  root: {
+    backgroundColor: "trnasparent",
+    height: "100vh",
+    display: "flex",
+    padding: "10px 0",
   },
 })(Container);
 
