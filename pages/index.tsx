34c1d88689935d6ae1d46f642d1a7f69a9bf04d2
Imanche Sabastine <imanchesebastian@gmail.com>
2020-06-29 02:28:47 -0700

    Added Box component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,92 @@
 import * as React from "react";
 import Head from "next/head";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
+import { deepOrange } from '@material-ui/core/colors';
 import Container from "@material-ui/core/Container";
+import CssBaseline from "@material-ui/core/CssBaseline/CssBaseline";
+import { Box, TextField, Checkbox } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
 
 const Home: React.FunctionComponent = () => {
   return (
-    <div className="container">
+    <div>
       <Head>
         <title>Postman</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <React.Fragment>
         <CssBaseline />
-        <Container fixed>
-        <Box display='flex'>
-          <Typography
-            component="div"
-            style={{ background: "url(./images/hero.svg)", height: "100vh", width: "50%" }}
+        <StyledContainer maxWidth="sm">
+          <Box height="600px" border='1px solid #ccc' padding='0 20px'>
+            <Box width='100%' height='100px' display='flex'>
+
+            </Box>
+          <label className='labels'>Email</label>
+          <StyledTextField id="outlined-basic" variant="outlined" fullWidth />
+          <label className='labels'>Username</label>
+          <StyledTextField id="outlined-basic" variant="outlined" fullWidth />
+          <label className='labels'>Password</label>
+          <StyledTextField id="outlined-basic" variant="outlined" fullWidth />
+          <Box width='100%' height='50px' display='flex'>
+          <StyledCheckBox
+            checked={true}
+            name="checkedB"
+            color="default"
           />
-          <Typography
-            component="div"
-            style={{ backgroundColor: "#000000", height: "100vh", width: "50%" }}
+          <label></label>
+            </Box>
+          <Box width='100%' height='50px' display='flex'>
+          <StyledCheckBox
+            checked={true}
+            name="checkedB"
+            color="default"
           />
-        </Box>
-        </Container>
+          <label></label>
+            </Box>
+          </Box>
+        </StyledContainer>
       </React.Fragment>
+
+      <style jsx global>{`
+        body {
+          margin: 0;
+          padding: 10px 0;
+          background-color: 'white';
+        }
+
+        .labels {
+          font-weight: bold;
+          font-size: '11px';
+        }
+      `}</style>
+
     </div>
   );
 };
 
+const StyledContainer = withStyles({
+  root: {
+    backgroundColor: 'white'
+  },
+})(Container);
 
-export default Home
+const StyledTextField = withStyles({
+  root: {
+    border: '.5px solid #ddd',
+    boxShadow: 'none',
+    margin: '10px 0'
+  },
+})(TextField);
+
+const StyledCheckBox = withStyles({
+  root: {
+    color:deepOrange[400],
+    '&$checked': {
+      color: deepOrange[600],
+  },
+  }
+})(Checkbox);
+
+
+
+export default Home;
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    entry: './pages/index.tsx',
+    output: {
+      path: __dirname + '/public',
+      filename: 'build/app.js'
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js']
+    },
+    module: {
+      rules: [
+        { test: /\.tsx?$/, loader: 'ts-loader' }
+      ]
+    }
+  }
